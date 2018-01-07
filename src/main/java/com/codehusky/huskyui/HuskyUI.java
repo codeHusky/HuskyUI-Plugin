0c2acbc1a56a0037a4710cf1a8f84854aa1a31a7
Loki <lokio27productions@gmail.com>
2018-01-06 23:18:05 -0600

    Fixed removal of inv items on view close

--- src/main/java/com/codehusky/huskyui/HuskyUI.java
+++ src/main/java/com/codehusky/huskyui/HuskyUI.java
@@ -53,7 +53,7 @@ public class HuskyUI {
     /**
      * The Version of HuskyUI for Sponge.
      */
-    public static final String PLUGIN_VERSION = "0.4.0";
+    public static final String PLUGIN_VERSION = "0.4.1";
 
     /**
      * The HuskyUI {@link Logger} used throughout the plugin.
--- src/main/java/com/codehusky/huskyui/InventoryUtil.java
+++ src/main/java/com/codehusky/huskyui/InventoryUtil.java
@@ -9,16 +9,11 @@ import java.util.Optional;
 
 public class InventoryUtil {
     public static void close(Player player) {
-        check(player.getOpenInventory()).ifPresent(Inventory::clear);
         player.closeInventory();
     }
 
     public static void open(Player player, Inventory inventory) {
-        check(player.getOpenInventory()).ifPresent(Inventory::clear);
         player.openInventory(inventory);
     }
 
-    public static Optional<Container> check(Optional<Container> inventory) {
-        return inventory.filter(i -> i.getProperties(StringProperty.class).stream().anyMatch(p -> p.equals(StringProperty.of("huskui-page"))));
-    }
 }
